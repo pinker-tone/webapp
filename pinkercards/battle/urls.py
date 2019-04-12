a860a2dbaa5c5756ea9e64d9190e058790fc1a60
HolidayMan <lesha.pitenko791@gmail.com>
2019-04-12 23:42:30 +0300

    Added class GameView that responses serialized data

--- pinkercards/battle/urls.py
+++ pinkercards/battle/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.urls import include
+from .views import *
 
 urlpatterns = [
-    # path('games/', include('api.urls')),
+    path('games/<str:username>/', GameView.as_view()),
 ]
--- pinkercards/battle/views.py
+++ pinkercards/battle/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions
+
+from .models import Game
+from .serializers import GameSerializer
+
+class GameView(APIView):
+	"""Комнаты чата"""
+
+	def get(self, request, username):
+		games = Game.objects.filter(user_1__username=username) | Game.objects.filter(user_2__username=username)
+		serializer = GameSerializer(games, many=True)
+		return Response({"data": serializer.data})
