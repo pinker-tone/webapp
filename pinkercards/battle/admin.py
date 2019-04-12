c45a5a0d06e3e9b048e29ba4e0d8ed3090bce81a
HolidayMan <lesha.pitenko791@gmail.com>
2019-04-12 23:39:25 +0300

    Added documentation and corrected list_display

--- pinkercards/battle/admin.py
+++ pinkercards/battle/admin.py
@@ -3,15 +3,18 @@ from .models import *
 
 
 class GameAdmin(admin.ModelAdmin):
-    """Комнаты чата"""
-    list_display = ("user_1", "user_2", "status")
+    """Игры"""
+    list_display = ("id", "user_1", "user_2", "status", "questions", "date")
+
+    def questions(self, obj):
+        return ', '.join([question.question_text for question in obj.all()[:5]])
 
     # def invited_user(self, obj):
     #     return "\n".join([user.username for user in obj.invited_users.all()])
 
 class QuestionsAdmin(admin.ModelAdmin):
-    """Комнаты чата"""
-    list_display = ("question_text", "answer", "question_type")
+    """Воросы"""
+    list_display = ("id", "question_text", "answer", "question_type")
 
 admin.site.register(Game, GameAdmin)
 admin.site.register(Questions, QuestionsAdmin)
