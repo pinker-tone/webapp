e33f1ec9841b6336b76169d82585b92d1c8e3f0e
HolidayMan <lesha.pitenko791@gmail.com>
2019-04-13 19:07:34 +0300

    added djoser urls

--- pinkercards/pinkercards/urls.py
+++ pinkercards/pinkercards/urls.py
@@ -19,5 +19,8 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api_v1/', include('battle.urls')),
+    path('api/v1/', include('battle.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
