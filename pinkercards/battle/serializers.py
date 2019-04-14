ccf85ea3de2d3e4cac4ad1b8c11f760ac29db309
HolidayMan <lesha.pitenko791@gmail.com>
2019-04-14 15:10:59 +0300

    added GameHistorySerializer

--- pinkercards/battle/serializers.py
+++ pinkercards/battle/serializers.py
@@ -1,26 +1,30 @@
 from rest_framework import serializers
-from .models import Game, Questions
+from .models import Game, Questions, GameHistory
 from django.contrib.auth.models import User
+
 
 class UserSerializer(serializers.ModelSerializer):
     """Сериализация таблицы юзеров"""
 
     class Meta:
         model = User
-        fields = ("id", "username")
+        fields = ("id", "username", "email")
+
 
 class QuestionsSerializer(serializers.ModelSerializer):
     """Сериализация таблицы вопросов"""
 
     class Meta:
         model = Questions
-        fields = ("id", "question_text")
+        fields = ("id", "question_text", "question_type", "answer")
+
 
 class GameSerializer(serializers.ModelSerializer):
     """Сериализация таблицы игр"""
     user_1 = UserSerializer()
     user_2 = UserSerializer()
     questions = QuestionsSerializer(many=True)
+
     class Meta:
         model = Game
         fields = ("id", "user_1", "user_2", "questions", "status", "date")
@@ -30,6 +34,15 @@ class GamePostSerializer(serializers.ModelSerializer):
     user_1 = UserSerializer()
     user_2 = UserSerializer()
     questions = QuestionsSerializer(many=True)
+
     class Meta:
         model = Game
         fields = ("user_1", "user_2")
+
+
+class GameHistorySerializer(serializers.ModelSerializer):
+    game = GameSerializer()
+    winner = UserSerializer()
+    class Meta:
+        model = GameHistory
+        fields = ("game", "winner", "answers_correct_user_1", "answers_correct_user_2")
