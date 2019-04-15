b90c073ad33b2e83c3eaf182011746f5f4f970e4
HolidayMan <lesha.pitenko791@gmail.com>
2019-04-15 21:48:50 +0300

    deleted game history serializer

--- pinkercards/battle/serializers.py
+++ pinkercards/battle/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Game, Questions, GameHistory
+from .models import Game, Questions
 from django.contrib.auth.models import User
 
 
@@ -27,9 +27,9 @@ class GameSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Game
-        fields = ("id", "user_1", "user_2", "questions", "status", "date")
+        fields = ("id", "user_1", "user_2", "questions", "status", "winner", "answers_correct_user_1", "answers_correct_user_2", "draw", "date")
 
-class GamePostSerializer(serializers.ModelSerializer):
+class GameCreateSerializer(serializers.ModelSerializer):
     """Сериализация таблицы игр"""
     user_1 = UserSerializer()
     user_2 = UserSerializer()
@@ -38,11 +38,3 @@ class GamePostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Game
         fields = ("user_1", "user_2")
-
-
-class GameHistorySerializer(serializers.ModelSerializer):
-    game = GameSerializer()
-    winner = UserSerializer()
-    class Meta:
-        model = GameHistory
-        fields = ("id","game", "winner", "answers_correct_user_1", "answers_correct_user_2")
